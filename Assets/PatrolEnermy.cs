fc5c0b87b394b609344b50d12b0af8ab308061cc
Trinh Nhan <147188783+trinhhnhann@users.noreply.github.com>
2025-06-27 22:59:23 +0700

    04:01:46  Player damage & scripting

--- Assets/PatrolEnermy.cs
+++ Assets/PatrolEnermy.cs
@@ -2,33 +2,85 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class PatrolEnermy : MonoBehaviour
-{   public float moveSpeed = 2f; // Speed of the enemy movement
-    public Transform checkPoint; // Point to check for ground
-    public float distance = 0.5f; // Distance to check for ground
-    public LayerMask layerMask; // Layer mask to specify which layers to check for ground
-    public bool facingLeft = true; // Direction the enemy is facing
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+{   public float moveSpeed = 2f;
+    public Transform checkPoint; 
+    public float distance = 0.5f;
+    public LayerMask layerMask; 
+    public bool facingLeft = true; 
+    public bool inRange = false;
+    public Transform player;
+    public float attackRange = 10f;
+    public float retrieveDistance = 2.5f;
+    public float chaseSpeed = 4f;
+    public Animator animator;
+    public Transform attackPoint; 
+    public float attackRadius = 1f; 
+    public LayerMask attackLayer;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
-
-        RaycastHit2D hit = Physics2D.Raycast(checkPoint.position, Vector2.down, distance, layerMask);
-
-        if (hit == false && facingLeft)
+        if (Vector2.Distance(transform.position, player.position) <= attackRange)
         {
-            transform.eulerAngles = new Vector3(0, -180, 0);
-            facingLeft = false;
+            inRange = true;
         }
-        else if (hit == false && facingLeft == false)
+        else
         {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-            facingLeft = true;
+            inRange = false;
+        }
+        if(inRange)
+        {
+
+            if(player.position.x > transform.position.x && facingLeft == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                facingLeft = false;
+            }else if(player.position.x < transform.position.x && facingLeft == false)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                facingLeft = true;
+            }
+
+            if (Vector2.Distance(transform.position, player.position) > retrieveDistance)
+            {
+                animator.SetBool("Attack1", false);
+                transform.position = Vector2.MoveTowards(transform.position, player.position, chaseSpeed * Time.deltaTime);
+            }
+            else
+            {
+                animator.SetBool("Attack1", true);
+            }
+        }
+        else
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+
+            RaycastHit2D hit = Physics2D.Raycast(checkPoint.position, Vector2.down, distance, layerMask);
+
+            if (hit == false && facingLeft)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                facingLeft = false;
+            }
+            else if (hit == false && facingLeft == false)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                facingLeft = true;
+            }
+        }
+        
+    }
+
+    public void Attack()
+    {
+        Collider2D collInfo = Physics2D.OverlapCircle(attackPoint.position, attackRadius, attackLayer);
+        if (collInfo)
+        {
+            Debug.Log(collInfo.transform.name);
         }
     }
 
@@ -41,5 +93,13 @@ public class PatrolEnermy : MonoBehaviour
 
         Gizmos.color = Color.yellow;
         Gizmos.DrawRay(checkPoint.position, Vector2.down * distance);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        if (attackPoint == null) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
     }
+
+
+    
 }
